e64090fb8816e5bec0071ad702262a40b5b83a5e
devgioele <gioeledevittidev@gmail.com>
2022-01-10 22:01:24 +0100

    feature: use MUI5's styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../src/theming/theme';
+import createEmotionCache from '../src/styling/createEmotionCache';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+type MyAppProps = AppProps & {
+  emotionCache?: EmotionCache;
+};
+
+export default function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>devgioele</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-
-export default MyApp
