081f619b74361af43c30c0e7351418a91c52c526
devgioele <gioeledevittidev@gmail.com>
2022-01-11 01:24:02 +0100

    feature: add footer

--- src/modules/organisms/Footer.tsx
+++ src/modules/organisms/Footer.tsx
@@ -1,3 +1,15 @@
-export default function Footer() {
-  return <footer>Footer</footer>;
+import Container, { ContainerProps } from '@mui/material/Container';
+
+type FooterProps = Pick<ContainerProps, 'maxWidth'> & {
+  width: string;
+};
+
+export default function Footer({ maxWidth, width }: FooterProps) {
+  return (
+    <footer>
+      <Container maxWidth={maxWidth} sx={{ width }}>
+        Footer
+      </Container>
+    </footer>
+  );
 }
