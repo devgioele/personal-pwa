ad71155aae389816412729ef653a4b9492478157
devgioele <gioeledevittidev@gmail.com>
2022-01-11 10:33:50 +0100

    feature: use MUI Typography

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,98 @@
 import type { NextPage } from 'next';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
     <Container maxWidth="lg">
-      <h1>Home Page</h1>
-      <p>
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
-          laboriosam libero sed sit unde! Alias, pariatur perferendis. Aliquam
-          cum id in, itaque labore magni, quas recusandae repudiandae rerum
-          similique tenetur.
-        </span>
-        <span>
-          Alias doloribus iure natus quaerat sit! Nam non nostrum odio.
-          Accusamus adipisci consequatur ipsum molestiae reiciendis. Ab
-          consequatur deserunt enim impedit ipsum officiis optio quasi
-          repudiandae, tempore, ullam vel, voluptatibus!
-        </span>
-        <span>
-          Ad, amet delectus deleniti dolor laudantium magni molestiae
-          necessitatibus porro quaerat quasi repellendus ullam voluptatem.
-          Accusamus ad atque autem deserunt explicabo id, incidunt ipsa officia
-          provident qui, sapiente ut veritatis.
-        </span>
-        <span>
-          Accusantium dolorum hic inventore laboriosam necessitatibus nesciunt
-          quas quidem quos saepe sed, sequi, tempora vel vero. Autem culpa
-          ducimus fuga iste magni minima molestiae nisi, optio reprehenderit
-          sint vitae voluptate?
-        </span>
-        <span>
-          Eaque mollitia quis similique. Ad at commodi dignissimos dolores
-          dolorum eos facere impedit, laboriosam laborum minima natus numquam
-          ratione rem repudiandae soluta ullam unde, vitae. Atque excepturi
-          libero odio reiciendis.
-        </span>
-        <span>
-          Animi aperiam aut consequatur culpa deserunt eligendi error eveniet ex
-          id ipsam mollitia neque quae quas quasi tempora, vel voluptas! Alias
-          aliquam animi commodi corporis ipsa iusto necessitatibus quo sit.
-        </span>
-        <span>
-          Aspernatur est eum ipsa molestias qui! Accusamus aliquam aliquid
-          assumenda cumque cupiditate delectus deleniti ea ipsa iste laborum
-          maiores necessitatibus nesciunt non, nostrum praesentium quod tenetur
-          ullam veniam veritatis vitae.
-        </span>
-        <span>
-          Accusantium adipisci aliquid, animi aperiam atque commodi deleniti
-          doloribus ex exercitationem illo ipsa laudantium, libero mollitia nisi
-          non nostrum numquam quia quibusdam quisquam rem sed similique velit
-          vero vitae voluptatum.
-        </span>
-        <span>
-          Autem consectetur excepturi in inventore necessitatibus quaerat quasi
-          recusandae reiciendis sed. A accusantium aliquid atque, cumque enim
-          ipsam laborum quam repellat. Dolorum ea ex nihil quibusdam, quis
-          ratione reiciendis voluptates.
-        </span>
-        <span>
-          Ab amet doloremque eos impedit iste magnam maiores mollitia, natus
-          nemo, quae quia quod quos veritatis? Accusantium aliquid labore
-          molestiae nostrum optio qui sed, soluta vero. Assumenda doloribus odio
-          placeat.
-        </span>
-        <span>
-          At consequuntur earum necessitatibus porro, sunt ullam voluptatibus?
-          Aspernatur assumenda beatae distinctio earum incidunt, itaque minus
-          nisi odit pariatur provident recusandae unde. Architecto eligendi
-          maxime officiis quam temporibus. Consequatur, consequuntur!
-        </span>
-        <span>
-          Alias aut eaque eligendi in ipsum mollitia, nulla repudiandae! Aliquam
-          architecto atque commodi, corporis ex incidunt ipsa laudantium,
-          necessitatibus nihil, officiis placeat repellat soluta tempora
-          temporibus tenetur veritatis voluptatem. Exercitationem.
-        </span>
-        <span>
-          Aliquid blanditiis cum, dignissimos eos iusto minima qui ut vel!
-          Consectetur cum dolorem, dolorum possimus qui veniam vitae?
-          Accusantium amet atque deleniti dolorem ducimus eveniet iste quam vero
-          voluptatem voluptates.
-        </span>
-        <span>
-          Blanditiis consequatur debitis dignissimos distinctio dolorem
-          doloremque impedit iure iusto molestias mollitia necessitatibus non,
-          officiis perferendis quaerat quos rem tempora temporibus vel veritatis
-          voluptas? Architecto blanditiis ipsam recusandae sequi velit?
-        </span>
-        <span>
-          Ab architecto cum deserunt dignissimos doloremque enim est et eveniet
-          fugiat impedit in neque nesciunt nisi nulla provident, quidem quisquam
-          quo rerum sed veniam. Aspernatur consectetur deserunt quibusdam sed
-          voluptates.
-        </span>
-      </p>
+      <Typography variant="h1">Hi!</Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        laboriosam libero sed sit unde! Alias, pariatur perferendis. Aliquam cum
+        id in, itaque labore magni, quas recusandae repudiandae rerum similique
+        tenetur.
+      </Typography>
+      <Typography variant="body1">
+        Alias doloribus iure natus quaerat sit! Nam non nostrum odio. Accusamus
+        adipisci consequatur ipsum molestiae reiciendis. Ab consequatur deserunt
+        enim impedit ipsum officiis optio quasi repudiandae, tempore, ullam vel,
+        voluptatibus!
+      </Typography>
+      <Typography variant="body1">
+        Ad, amet delectus deleniti dolor laudantium magni molestiae
+        necessitatibus porro quaerat quasi repellendus ullam voluptatem.
+        Accusamus ad atque autem deserunt explicabo id, incidunt ipsa officia
+        provident qui, sapiente ut veritatis.
+      </Typography>
+      <Typography variant="body1">
+        Accusantium dolorum hic inventore laboriosam necessitatibus nesciunt
+        quas quidem quos saepe sed, sequi, tempora vel vero. Autem culpa ducimus
+        fuga iste magni minima molestiae nisi, optio reprehenderit sint vitae
+        voluptate?
+      </Typography>
+      <Typography variant="body1">
+        Eaque mollitia quis similique. Ad at commodi dignissimos dolores dolorum
+        eos facere impedit, laboriosam laborum minima natus numquam ratione rem
+        repudiandae soluta ullam unde, vitae. Atque excepturi libero odio
+        reiciendis.
+      </Typography>
+      <Typography variant="body1">
+        Animi aperiam aut consequatur culpa deserunt eligendi error eveniet ex
+        id ipsam mollitia neque quae quas quasi tempora, vel voluptas! Alias
+        aliquam animi commodi corporis ipsa iusto necessitatibus quo sit.
+      </Typography>
+      <Typography variant="body1">
+        Aspernatur est eum ipsa molestias qui! Accusamus aliquam aliquid
+        assumenda cumque cupiditate delectus deleniti ea ipsa iste laborum
+        maiores necessitatibus nesciunt non, nostrum praesentium quod tenetur
+        ullam veniam veritatis vitae.
+      </Typography>
+      <Typography variant="body1">
+        Accusantium adipisci aliquid, animi aperiam atque commodi deleniti
+        doloribus ex exercitationem illo ipsa laudantium, libero mollitia nisi
+        non nostrum numquam quia quibusdam quisquam rem sed similique velit vero
+        vitae voluptatum.
+      </Typography>
+      <Typography variant="body1">
+        Autem consectetur excepturi in inventore necessitatibus quaerat quasi
+        recusandae reiciendis sed. A accusantium aliquid atque, cumque enim
+        ipsam laborum quam repellat. Dolorum ea ex nihil quibusdam, quis ratione
+        reiciendis voluptates.
+      </Typography>
+      <Typography variant="body1">
+        Ab amet doloremque eos impedit iste magnam maiores mollitia, natus nemo,
+        quae quia quod quos veritatis? Accusantium aliquid labore molestiae
+        nostrum optio qui sed, soluta vero. Assumenda doloribus odio placeat.
+      </Typography>
+      <Typography variant="body1">
+        At consequuntur earum necessitatibus porro, sunt ullam voluptatibus?
+        Aspernatur assumenda beatae distinctio earum incidunt, itaque minus nisi
+        odit pariatur provident recusandae unde. Architecto eligendi maxime
+        officiis quam temporibus. Consequatur, consequuntur!
+      </Typography>
+      <Typography variant="body1">
+        Alias aut eaque eligendi in ipsum mollitia, nulla repudiandae! Aliquam
+        architecto atque commodi, corporis ex incidunt ipsa laudantium,
+        necessitatibus nihil, officiis placeat repellat soluta tempora
+        temporibus tenetur veritatis voluptatem. Exercitationem.
+      </Typography>
+      <Typography variant="body1">
+        Aliquid blanditiis cum, dignissimos eos iusto minima qui ut vel!
+        Consectetur cum dolorem, dolorum possimus qui veniam vitae? Accusantium
+        amet atque deleniti dolorem ducimus eveniet iste quam vero voluptatem
+        voluptates.
+      </Typography>
+      <Typography variant="body1">
+        Blanditiis consequatur debitis dignissimos distinctio dolorem doloremque
+        impedit iure iusto molestias mollitia necessitatibus non, officiis
+        perferendis quaerat quos rem tempora temporibus vel veritatis voluptas?
+        Architecto blanditiis ipsam recusandae sequi velit?
+      </Typography>
+      <Typography variant="body1">
+        Ab architecto cum deserunt dignissimos doloremque enim est et eveniet
+        fugiat impedit in neque nesciunt nisi nulla provident, quidem quisquam
+        quo rerum sed veniam. Aspernatur consectetur deserunt quibusdam sed
+        voluptates.
+      </Typography>
     </Container>
   );
 };
