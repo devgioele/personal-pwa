f7e9d232b663aa40c1629ffacb84075bddd880d6
devgioele <gioeledevittidev@gmail.com>
2022-01-11 10:08:23 +0100

    refactor: rm src directory

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
-import createEmotionCache from 'styling/createEmotionCache';
-import theme from 'theming/theme';
+import createEmotionCache from '../styling/createEmotionCache';
+import theme from '../theming/theme';
 
 type MyDocumentAdditionalProps = {
   emotionStyleTags: JSX.Element[];
