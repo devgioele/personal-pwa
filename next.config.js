2b6c36967d034d9ae0e200fa610aeb08ee75c48c
devgioele <gioeledevittidev@gmail.com>
2022-01-13 21:24:41 +0100

    feature: add template of http header for onion redirection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  experimental: {
+    outputStandalone: true,
+  },
+  // See the docs: https://nextjs.org/docs/api-reference/next.config.js/headers
+  async headers() {
+    return [
+      {
+        // Match all paths
+        source: '/:path*',
+        headers: [
+          {
+            key: 'onion-location',
+            // TODO: Enter correct onion address
+            value: 'https://temp.onion',
+          },
+        ],
+      },
+    ];
+  },
+};
