89ee511b8f769110fc80155eaa80d2444ad23255
devgioele <gioeledevittidev@gmail.com>
2022-01-19 00:10:37 +0100

    fix: use sudo for compute instance scripts

--- deployment/terraform/setup.sh
+++ deployment/terraform/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Setup the package manager
-apt update
-apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update
+sudo apt update
+sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
 
 # Install docker
-apt install --yes docker-ce
+sudo apt install --yes docker-ce
 # Install docker-compose
 # curl -L "https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # chmod +x /usr/local/bin/docker-compose
