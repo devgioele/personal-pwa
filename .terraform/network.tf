605140cbc6f6dd4209faa3329ca6ff5011098aff
devgioele <gioeledevittidev@gmail.com>
2022-01-18 12:10:20 +0100

    feature: allow ssh for compute network

--- .terraform/network.tf
+++ .terraform/network.tf
@@ -1,10 +1,17 @@
 resource "google_compute_firewall" "default" {
-  name    = "default-firewall"
+  name = "default-firewall"
   network = google_compute_network.default.name
 
+  # HTTP and HTTPS
   allow {
     protocol = "tcp"
-    ports    = ["80", "443"]
+    ports = ["80", "443"]
+  }
+
+  # SSH
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
   }
 
   source_tags = ["web"]
