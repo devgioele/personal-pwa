05189723f4ad2d9c234487aa4e579686d871915a
devgioele <gioeledevittidev@gmail.com>
2022-01-18 12:30:25 +0100

    feature: allow TF to stop the instance for updates

--- deployment/terraform/compute.tf
+++ deployment/terraform/compute.tf
@@ -3,10 +3,12 @@ data "google_compute_zones" "available" {
 }
 
 resource "google_compute_instance" "default" {
-  project      = google_project.project.project_id
-  zone         = data.google_compute_zones.available.names[0]
-  name         = "tf-compute-1"
+  project = google_project.project.project_id
+  zone = data.google_compute_zones.available.names[0]
+  name = "tf-compute-1"
   machine_type = var.gcp_compute_machine_type
+  # Allow Terraform to stop the instance to update its properties
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
@@ -18,6 +20,9 @@ resource "google_compute_instance" "default" {
     network = "default"
     access_config {}
   }
+
+  # Causes a recreation of the resource if changed
+  metadata_startup_script = file(var.compute_startup)
 
   depends_on = [google_project_service.service]
 }
