87d4b7d3fb32e032c65f36df8e4440b5ef40dbed
devgioele <gioeledevittidev@gmail.com>
2022-01-19 12:29:15 +0100

    fix: pass docker container IDs correctly

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Remove running containers
-sudo docker rm -f "$(docker ps -a -q)"
+# shellcheck disable=SC2046
+sudo docker rm -f $(sudo docker ps -a -q)
 # Take TOR private key and export it
 export TOR_PRIVATE_KEY="$1"
 # Build and start containers
