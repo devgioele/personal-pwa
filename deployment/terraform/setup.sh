61718cf91395d65029819c0f4b0eeaaf5344529f
devgioele <gioeledevittidev@gmail.com>
2022-01-19 01:58:06 +0100

    fix: install docker instead of community edition

--- deployment/terraform/setup.sh
+++ deployment/terraform/setup.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debi
 sudo apt update
 
 # Install docker
-sudo apt install --yes docker-ce
+sudo apt install --yes docker
 # Install docker-compose
 # curl -L "https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # chmod +x /usr/local/bin/docker-compose
