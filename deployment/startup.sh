cbaa1ca9728c3aeb0138ddd68cd5c918c09b52af
devgioele <gioeledevittidev@gmail.com>
2022-01-19 22:34:15 +0100

    fix: preserve env vars when running docker-compose with sudo

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -17,4 +17,4 @@ sudo docker rm -f $(sudo docker ps -a -q)
 # Take TOR private key and export it
 export TOR_PRIVATE_KEY="$1"
 # Build and start containers
-sudo docker-compose -f "$2" up -d
+sudo --preserve-env=TOR_PRIVATE_KEY docker-compose -f "$2" up -d
